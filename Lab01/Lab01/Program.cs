e385760ba34e3118f9748bb0d378ea422c0ee986
Mariusz Szymański <s30378@pjwstk.edu.pl>
2025-03-07 16:43:50 +0100

    Final change

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, my first change!!!");
 Console.WriteLine("Hello, my second change!!!");
+Console.WriteLine("Hello, my final change!!!");
